04e56b1b3a0ea5ea8ffd40c2af1dda6f48853f4c
Linwl <304115325@qq.com>
2019-03-07 22:36:06 +0800

    完成epoll主循环的流程

--- epoll_lo_loop.py
+++ epoll_lo_loop.py
@@ -19,7 +19,36 @@ class EpollIOLoop:
         self.server.setblocking(False)#设置为非阻塞
         self.epoll = select.epoll()
         self.epoll.register(self.server.fileno(),select.EPOLLIN)
+        self.connections={}
+        self.requests={}
+        self.responses={}
 
     def start(self):
         while 1:
-            pass
+            events = self.epoll.epoll()
+            for fileno,event in events:
+                if fileno == self.server.fileno():
+                    print('有客户连接进入')
+                    connection, addr = self.server.accept()
+                    connFd = connection.fileno()#获取连接句柄
+                    connection.setblocking(False)
+                    self.epoll.register(connFd, select.EPOLLIN)#注册epoll的读事件
+                    self.connections[connFd] = connection
+                elif event & select.EPOLLHUP:
+                    print('客户连接已断开')
+                    # 销毁已断开的客户端socket句柄
+                    self.epoll.unregister(fileno)
+                    # 关闭客户端socket连接
+                    self.connections[fileno].close()
+                    #剔除该客户端
+                    self.connections.pop(fileno)
+                elif event & select.EPOLLIN:
+                    # 接收数据
+                    self.requests[fileno] = self.connections[fileno].recv(1024).strip()
+                    # 将客户端文件句柄整形的从EPOLLIN中转移到 EPOLLOUT
+                    self.epoll.modify(fileno, select.EPOLLOUT)
+                elif event & select.EPOLLOUT:
+                    # 发送数据
+                    self.connections[fileno].send(self.requests[fileno])
+                    # 将客户端文件句柄整形的 从EPOLLOUT中转移到 EPOLLIN
+                    self.epoll.modify(fileno, select.EPOLLIN)
